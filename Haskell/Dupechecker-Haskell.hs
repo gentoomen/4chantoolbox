24714b0c1de9276b1fbb823e860e60c9865385fd
LAMMJohnson <john_anthony@lavabit.com>
2011-02-21 18:47:34 +0000

    Really basic stuff

--- Haskell/Dupechecker-Haskell.hs
+++ Haskell/Dupechecker-Haskell.hs
@@ -1,1 +1,6 @@
-main = putStrLn "Hello, World!"
+import System.Directory (getDirectoryContents)
+import System.IO
+
+lisFiles = getDirectoryContents "/home/john/Pictures/Dupes"
+
+main = print "Protype Stage"
