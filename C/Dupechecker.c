a847c7b2aebbdea506395b1bcbc3a327329ab8b6
LAMMJohnson <john_anthony@lavabit.com>
2011-03-13 13:37:21 +0000

    Framework for input handling on match

--- C/Dupechecker.c
+++ C/Dupechecker.c
@@ -34,6 +34,7 @@ void errout(char* str);
 char* get_full_path(char* path, char* forf);
 char* get_hash(fileLL *f);
 void handleargs(int argc, char** argv);
+void handle_match(fileLL *f, fileLL *fc);
 int is_dir(char* path);
 void show_match(fileLL *a, fileLL *b);
 void usage(void);
@@ -88,7 +89,9 @@ compare_files(void) {
                     fc->md5hash = get_hash(fc);
 
                 show_match(f, fc);
-                if(strcmp(f->md5hash, fc->md5hash))
+                if(!strcmp(f->md5hash, fc->md5hash))
+                    handle_match(f, fc);
+                else
                     puts("But hashes do not match.");
             }
             fc = fc->next;
@@ -152,6 +155,35 @@ handleargs(int argc, char** argv) {
         else
             printf("Unrecognised command/is not a valid path: %s\nContinuing anyway.\n", argv[i]);
     }
+}
+
+void
+handle_match(fileLL *f, fileLL *fc) {
+    char c;
+
+    if (dummy){
+        puts("=== Full md5 match found ===");
+        return;
+    }
+
+    printf( "Match found! Would you like to delete duplicate file %s ?\n", fc->path );
+
+    c = getchar();
+
+    /*     else: */
+    /*         print "Match found! Would you like to delete duplicate file " + startfile.wholepath + "" */
+
+    /*             # Allowance for the automatic overwrite flag */
+    /*             if ask: */
+    /*                 choice = get_choice() */
+
+    /*                     # In instructed to, delete the file     */
+    /*                     if choice in ('o', 'other', 'b', 'both'): */
+    /*                         remfile(checkagainst) */
+    /*                         if choice in ('y', 'yes', 'b', 'both') or not ask: */
+    /*                             remfile(startfile) */
+    /*                             break */
+
 }
 
 int
@@ -221,31 +253,19 @@ int main(int argc, char** argv) {
     if (!is_dir(workdir))
         errout("Failed to open given directory.");
 
+    /* Spit out a warning if we're using the current dir */
+    if (!strcmp(workdir, "./"))
+        puts("No valid directory given. Using current directory.");
+
     work_through_dir(workdir);
     
     compare_files();
 
+    puts("Checking finished. Exiting successfully.");
+
     return 0;
 }
 /*
-class fileblock():
-    def __init__(self, inpath):
-        self.wholepath = inpath
-        self.size = os.path.getsize(inpath)
-        self.md5 = ""
-
-    def get_md5(self):
-        print "Generating md5 for " + self.wholepath
-        self.md5 = md5_for_file(self.wholepath)
-
-def add_dir(dirpath):
-    print "Scanning directory " + dirpath
-    for tempfile in os.listdir(dirpath):
-        if os.path.isdir(dirpath + tempfile) and recursive:
-            add_dir(dirpath + tempfile + "/")
-        elif os.path.isfile(dirpath + tempfile):
-            filestats.append( fileblock( dirpath + tempfile ) )
-
 # get user input to determine if it's deleteion time or not
 def get_choice():
     choose = ""
@@ -253,16 +273,6 @@ def get_choice():
     while not choose in validOptions :
         choose = raw_input('(y)es / (n)o / the (o)ther file / (b)oth ? ')
     return choose
-
-def md5_for_file(filename, block_size=2**20):
-    f = open(filename)
-    md5 = hashlib.md5()
-    while True:
-        data = f.read(block_size)
-        if not data:
-            break
-        md5.update(data)
-    return md5.digest()
 
 def pretty_size(size):
     suffixes = [("B",2**10), ("K",2**20), ("M",2**30), ("G",2**40), ("T",2**50)]
@@ -276,55 +286,6 @@ def remfile(infile):
     if os.path.isfile(infile.wholepath):
         os.remove(infile.wholepath)
         print infile.wholepath + " deleted."
-
-def showmatch(filea, fileb):
-    print "=========================== Size Match ==========================="
-    print "File:    " + filea.wholepath
-    print "Matches: " + fileb.wholepath
-    print "Size:    " + pretty_size( filea.size )
-
-def usage():
-    print "=================================================================="
-    print "Usage: Dupechecker-Python [OPTION] <directory>"
-    print "Script to remove duplicate files"
-    print " "
-    print " -d/--dummy        No deletion. Output findings without prompting"
-    print " -r/--recursive    Recurse through directories"
-    print " -y/--yes-to-all   Remove duplicates without prompting"
-    print " -h/--help         This help text"
-    print "=================================================================="
-    sys.exit(' ')
-
-## Rip out any genuine paths in the args
-for arg in sys.argv:
-    if os.path.isdir(arg):
-        directory = arg
-
-## Handle Args
-try:
-    opts, args = getopt.gnu_getopt( sys.argv, "dhry", ["dummy", "help", "recursive", "yes-to-all"] )
-
-except getopt.GetoptError:
-    print str(err)
-    usage()
-
-for o, a in opts:
-    if o in ("-d", "--dummy"):
-        dummy = True
-    elif o in ("-r", "--recursive"):
-        recursive = True
-    elif o in ("-y", "--yes-to-all"):
-        ask = False
-    elif o in ("-h", "--help"):
-        usage()
-
-# Spit out a warnign if we're using the current dir
-if directory == "./":
-    print "No valid directory given. Using current directory."
-
-# If the dir doesn't end with a "/" we need to add one
-if not directory[ len(directory) - 1 ] == "/":
-    directory = directory + "/"
 
 ## Main part
 #Get a list of files only (no dirs)
@@ -350,23 +311,6 @@ for i in range(len(filestats)):
                         if f.md5 == "":
                             f.get_md5()
                 
-                    if startfile.md5 == checkagainst.md5:
-                        if dummy:
-                            print "=== Full md5 match found ==="
-
-                        else:
-                            print "Match found! Would you like to delete duplicate file " + startfile.wholepath + ""
-    
-                            # Allowance for the automatic overwrite flag
-                            if ask:
-                                choice = get_choice()
-
-                            # In instructed to, delete the file    
-                            if choice in ('o', 'other', 'b', 'both'):
-                                remfile(checkagainst)
-                            if choice in ('y', 'yes', 'b', 'both') or not ask:
-                                remfile(startfile)
-                                break
                     else:
                         print "No md5 match."
 */
