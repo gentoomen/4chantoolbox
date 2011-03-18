d98fc9be107dac6839f45cae7acb905739dd6215
LAMMJohnson <tekkub@gmail.com>
2011-03-18 17:07:15 +0000

    Added initial data processing

--- C/Scraper.c
+++ C/Scraper.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
 
+/* Datatypes */
+struct MemoryStruct {
+  char *memory;
+  size_t size;
+};
+
 /* Globals */
-CURL        *curl;
-CURLcode    res;
-char        *URL;
+CURL                    *curl_handle;
+CURLcode                res;
+char                    *URL;
+struct MemoryStruct     URLdata;
 
 /* Predecs */
 void errout(char* str);
 void handleargs(int argc, char** argv);
 void usage(void);
 
+/* Functions */
 void
 errout(char* str) {
     if(str)
@@ -36,8 +45,28 @@ usage(void) {
     exit(0);
 }
 
+static size_t
+WriteMemoryCallback(void *ptr, size_t size, size_t nmemb, void *data)
+{
+  size_t realsize = size * nmemb;
+  struct MemoryStruct *mem = (struct MemoryStruct *)data;
+ 
+  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+  if (mem->memory == NULL) {
+    /* out of memory! */ 
+    printf("not enough memory (realloc returned NULL)\n");
+    exit(EXIT_FAILURE);
+  }
+ 
+  memcpy(&(mem->memory[mem->size]), ptr, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+ 
+  return realsize;
+}
+
 int main(int argc, char** argv) {
-    curl = curl_easy_init();
+    curl_handle = curl_easy_init();
 
     if(argc < 2) {
         puts("Not enough arguments given.");
@@ -46,13 +75,22 @@ int main(int argc, char** argv) {
     else
         handleargs(argc, argv);
 
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, URL);
-        res = curl_easy_perform(curl);
+    if(curl_handle) {
+        curl_easy_setopt(curl_handle, CURLOPT_URL, URL);
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void*) &URLdata);
+        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "Firefox/3.6.15");
 
-        printf("Grabbing URL: %s\n", URL);
+        curl_easy_perform(curl_handle);
 
-        curl_easy_cleanup(curl);
+        if(URLdata.memory) {
+            printf("Grabbed URL: %s\n", URL);
+            printf("Data:\n%s", URLdata.memory);
+
+            curl_easy_cleanup(curl_handle);
+        }
+        else
+            errout("Unable to grab URL.");
     }
 
     return 0;
